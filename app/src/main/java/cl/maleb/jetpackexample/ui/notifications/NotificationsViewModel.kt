f3a98c13ee32fec6ad645cc8cd40362f6d905497
maleb1337 <pe.tobar.m@gmail.com>
2021-06-07 16:03:20 -0400

    Initial commit

---
+++ app/src/main/java/cl/maleb/jetpackexample/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package cl.maleb.jetpackexample.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
